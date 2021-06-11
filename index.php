37e4c1a9cd1a154d7ffd09836a613d98aa288112
MundingAkbar <sl201501058@wmsu.edu.ph>
2021-06-11 21:42:24 +0800

    .

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/dogemonkey.png">
+    <link rel="stylesheet" href="style.css?v=1.1">
+    
+    <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js" integrity="sha256-XL2inqUJaslATFnHdJOi9GfQ60on8Wx1C2H8DYiN1xY=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/faf20bb39b.js" crossorigin="anonymous"></script>
+    <title>dogemonkey</title>
+</head>
+<body>
+    <div class="container">
+        <div id="heading">
+            <h2 id="title"><a href="#">Official Website of DogeMonkey</a></h2>
+            <div class="social_media">
+                <a href=""><i class="fab fa-facebook"></i></a>
+                <a href="https://www.twitter.com/DogeMonkeyD"><i class="fab fa-twitter"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/channel/UCb-9Q77xsCxmFaGrJEITmrw"><i class="fab fa-youtube"></i></a>
+                <a href="https://www.reddit.com/r/DogeMonkey/"><i class="fab fa-reddit-alien"></i></a>
+                <a href="t.me/DogeMonkeyOfficial"><i class="fab fa-telegram-plane"></i></a>
+            </div>
+        </div>
+        <img id="logo" src="img/dogemonkey.png" alt="dogemonkey">
+        <h1 id='label'>COMING SOON...</h1>
+    </div>
+</body>
+
+<script>
+    anime({
+        targets: '#label',
+        scale: 1.1,
+        loop: true,
+        easing: 'easeInOutSine'
+    });
+</script>
+</html>
