04f52ab120e4db0f1e16c6d2c6b2c8a1e0d0d9b2
MundingAkbar <sl201501058@wmsu.edu.ph>
2021-06-12 19:48:27 +0800

    .

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/dogemonkey.png">
-    <link rel="stylesheet" href="style.css?v=2.5">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     
     <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js" integrity="sha256-XL2inqUJaslATFnHdJOi9GfQ60on8Wx1C2H8DYiN1xY=" crossorigin="anonymous"></script>
@@ -13,6 +13,7 @@
     <title>dogemonkey</title>
 </head>
 <body>
+    <!-- first section -->
     <div class="container">
         <div id="heading">
             <h2 id="title"><a href="#">Official Website of DogeMonkey </a></h2>
@@ -51,45 +52,55 @@
             <img id="logo" src="img/dogemonkey.png" alt="dogemonkey">
         </section>
     </div>
+    <!-- end of first section -->
+    <!-- second section -->
+    <div class="container section2">
+            <div id="section2-header" data-aos="fade-up">
+                <img src="img/dogemonkey.png" alt="dogemonkey">
+                <h2 class="label" id="section2-title"></h2>
+            </div>
+            <div class="section2-content" data-aos="fade-left">
+                <p>$DM is a token on the Binance Smart Chain boasting a number of impressive features.<br>
+                A lot of trust and transparency are built into $DM, which is programmed to reward holders.</p>
+                <p>In order to achieve new heights, we intend to continue building on the work of previous<br>
+                projects. This is accomplished by combining an innovative auto-liquidity feature that increases token<br>
+                liquidity quickly, a large burn rate, and an incentive system to reward token holders. Together,<br> 
+                all of these features can create a token that is ready to contribute back to community.</p>
+                <h4><a href="#">WHITEPAPER (Releasing soon)</a></h4>
+            </div>
+    </div>
+    <!-- end of second section  -->
+    <div class="container section3">
+           <div class="section3-main-content">
+            <div class="section3-content" data-aos="fade-up">
+                    <h3>Community Driven Venture</h3>
+                    <p>DogeMonkey is a Community Driven venture
+                    and fair launched open to everyone. Percentage
+                    of the total supply has already been burnt as well
+                    as the developer's wallet. Like everyone else, the
+                    rest of the team participated in the fair launch.</p>
+                </div>
+                <div class="section3-content" data-aos="fade-up">
+                    <h3>Automatic Liquidation</h3>
+                    <p>The liquidity pool is enlarged with every transaction.
+                    Trades contribute to the automatic creation of liquidity that
+                    goes into multiple pools used by trading platforms or exchanges.
+                    We already burned 80% of $DM's total supply. This project is
+                    rugproof since nobody can access it and the ownership has been
+                    renounced.</p>
+                </div>
+                <div class="section3-content" data-aos="fade-up">
+                    <h3>Passive Rewards</h3>
+                    <p>DogeMonkey investors will receive passive rewards while holding
+                    onto their toens. Holders can watch their balance grow over time
+                    and on every transcation.</p>    
+                </div>
+           </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-        AOS.init({
-            offset: 120, 
-        });
-
-        function myFunction() {
-        /* Get the text field */
-        var copyText = document.getElementById("copy_add");
-
-        /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-        /* Copy the text inside the text field */
-        document.execCommand("copy");
-
-        /* Alert the copied text */
-        alert("Copied the text: " + copyText.value);
-        }
-
-        var btn_copy = document.getElementById("btn_copy");
-        btn_copy.addEventListener("click",myFunction)
-
-        anime({
-            targets: '#btn_buy',
-            scale: 1.05,
-            loop: true,
-            easing: 'easeInOutSine'
-        });
-
-        // This will work! 
-        new TypeIt("#title", {
-                speed: 50,
-                })
-                .pause(500)
-                .go();
-    </script>
+    <script src="main.js"></script>
     
 </body>
 </html>
